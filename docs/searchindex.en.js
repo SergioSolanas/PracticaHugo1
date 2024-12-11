51bfab6d95cb9229b52cd37a0941d97a45e91045
Sergio Solanas <sersovia@gmail.com>
2024-12-11 18:12:00 +0100

    despliegue

---
+++ docs/searchindex.en.js
@@ -0,0 +1,82 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Quién soy",
+    "content": "En esta sección voy a explicar un par de conceptos sobre Java: Java es el primer lenguaje de programación que aprendí y el que maás he utilizado, es un lenguaje orientado a objetos, lo que lo hace muy poderoso y estructurado.\nAlgunos ejemplos de código son los siguientes: Estructura Básica: Un programa en Java se compone de clases y métodos. El punto de entrada principal es el método main.\npublic class MiPrimeraClase { public static void main(String[] args) { System.out.println(\"¡Hola, mundo!\"); } } public class MiPrimeraClase: Declara una clase llamada MiPrimeraClase. public static void main(String[] args) : El método principal donde inicia la ejecución. System.out.println(\"¡Hola, mundo!\");: Imprime texto en la consola. Declaración de variables: Java es fuertemente tipado, lo que significa que cada variable debe tener un tipo definido.\nint numero = 10; // Entero double decimal = 5.5; // Número con decimales char letra = 'A'; // Carácter boolean esVerdad = true; // Booleano (verdadero/falso) String texto = \"Hola\"; // Cadena de texto Condicionales: Las estructuras condicionales se utilizan para tomar decisiones.\nint edad = 18; if (edad \u003e= 18) { System.out.println(\"Eres mayor de edad.\"); } else { System.out.println(\"Eres menor de edad.\"); } Bucles: Se utilizan para repetir bloques de código.\nBucle for: for (int i = 0; i \u003c 5; i++) { System.out.println(\"Iteración: \" + i); } Bucle while: int contador = 0; while (contador \u003c 5) { System.out.println(\"Contador: \" + contador); contador++; } Clases y Objetos: En Java, los programas se organizan en clases y se pueden crear objetos para utilizarlas.\npublic class Persona { String nombre; public Persona(String nombre) { // Constructor this.nombre = nombre; } public void saludar() { System.out.println(\"Hola, mi nombre es \" + nombre); } public static void main(String[] args) { Persona p = new Persona(\"Carlos\"); p.saludar(); } } Resultado:\nHola, mi nombre es Carlos",
+    "description": "En esta sección voy a explicar un par de conceptos sobre Java: Java es el primer lenguaje de programación que aprendí y el que maás he utilizado, es un lenguaje orientado a objetos, lo que lo hace muy poderoso y estructurado.\nAlgunos ejemplos de código son los siguientes: Estructura Básica: Un programa en Java se compone de clases y métodos. El punto de entrada principal es el método main.\npublic class MiPrimeraClase { public static void main(String[] args) { System.out.println(\"¡Hola, mundo!\"); } } public class MiPrimeraClase: Declara una clase llamada MiPrimeraClase. public static void main(String[] args) : El método principal donde inicia la ejecución. System.out.println(\"¡Hola, mundo!\");: Imprime texto en la consola. Declaración de variables: Java es fuertemente tipado, lo que significa que cada variable debe tener un tipo definido.",
+    "tags": [],
+    "title": "Sección Técnica: Java",
+    "uri": "/PracticaHugo1/java/index.html"
+  },
+  {
+    "breadcrumb": "Quién soy \u003e Markdown",
+    "content": "Encabezados En Markdown, los encabezados se crean utilizando el símbolo #. Puedes ajustar el tamaño del encabezado agregando más signos #, donde uno es el nivel más alto (H1) y seis es el nivel más bajo (H6).\nEjemplo: # Encabezado 1\r## Encabezado 2\r### Encabezado 3\r#### Encabezado 4\r##### Encabezado 5\r###### Encabezado 6 Resultado: Encabezado 1 Encabezado 2 Encabezado 3 Encabezado 4 Encabezado 5 Encabezado 6",
+    "description": "Encabezados En Markdown, los encabezados se crean utilizando el símbolo #. Puedes ajustar el tamaño del encabezado agregando más signos #, donde uno es el nivel más alto (H1) y seis es el nivel más bajo (H6).\nEjemplo: # Encabezado 1\r## Encabezado 2\r### Encabezado 3\r#### Encabezado 4\r##### Encabezado 5\r###### Encabezado 6 Resultado: Encabezado 1 Encabezado 2 Encabezado 3 Encabezado 4 Encabezado 5 Encabezado 6",
+    "tags": [],
+    "title": "Encabezados",
+    "uri": "/PracticaHugo1/markdown/encabezados/index.html"
+  },
+  {
+    "breadcrumb": "Quién soy",
+    "content": "Listado de canales de tv",
+    "description": "Listado de canales de tv",
+    "tags": [],
+    "title": "Hobbies",
+    "uri": "/PracticaHugo1/hobbies/index.html"
+  },
+  {
+    "breadcrumb": "Quién soy",
+    "content": "En esta sección voy a explicar un par de conceptos sobre la sintaxis de Markdown: Markdown es un lenguaje de marcado ligero que permite dar formato a texto de manera simple y rápida. Fue diseñado para que sea legible como texto plano y se convierta fácilmente en HTML. Es ampliamente utilizado en documentación, blogs y plataformas como GitHub.\nAlgunas características son: Encabezados: → Ejemplo Encabezados\nTablas: → Ejemplo Tablas\nImágenes: → Ejemplo Imágenes\nOtros: → Ejemplo Otros",
+    "description": "En esta sección voy a explicar un par de conceptos sobre la sintaxis de Markdown: Markdown es un lenguaje de marcado ligero que permite dar formato a texto de manera simple y rápida. Fue diseñado para que sea legible como texto plano y se convierta fácilmente en HTML. Es ampliamente utilizado en documentación, blogs y plataformas como GitHub.\nAlgunas características son: Encabezados: → Ejemplo Encabezados\nTablas: → Ejemplo Tablas\nImágenes: → Ejemplo Imágenes",
+    "tags": [],
+    "title": "Markdown",
+    "uri": "/PracticaHugo1/markdown/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "PRESENTACIÓN Mi nombre es: Juan Carlos Martínez Profesión: Periodista deportivo Me especializo en: Cobertura de eventos deportivos, análisis táctico y entrevistas exclusivas. Perfil Profesional: Apasionado por el deporte y la comunicación, Juan Carlos Martínez cuenta con más de 25 años de experiencia en el ámbito del periodismo deportivo. Ha trabajado para medios reconocidos como as.com, donde ha cubierto desde ligas locales hasta competiciones internacionales como la Copa del Mundo, los Juegos Olímpicos y los torneos más destacados de disciplinas como el fútbol, baloncesto y tenis.\nVoy a detallar mis habilidades: Narración y redacción: Crónicas deportivas que capturan la emoción del momento. Cobertura en vivo: Experto en transmisiones en directo desde estadios y arenas. Análisis profundo: Conocimientos avanzados sobre tácticas y estadísticas deportivas. Entrevistas exclusivas: Experiencia en interacciones con atletas, entrenadores y figuras clave del deporte. Manejo de medios digitales: Creación de contenido para redes sociales, blogs y podcasts deportivos. Listado de Logros:\nReconocido por Premio Nacional de Periodismo Deportivo Manuel Alcántara 2018 Entrevista exclusiva con Lionel Andrés Messi. Objetivo Profesional: Seguir inspirando a los aficionados al deporte a través de historias que resalten el esfuerzo, la pasión y el espíritu competitivo, explorando los límites del periodismo deportivo en la era digital.",
+    "description": "PRESENTACIÓN Mi nombre es: Juan Carlos Martínez Profesión: Periodista deportivo Me especializo en: Cobertura de eventos deportivos, análisis táctico y entrevistas exclusivas. Perfil Profesional: Apasionado por el deporte y la comunicación, Juan Carlos Martínez cuenta con más de 25 años de experiencia en el ámbito del periodismo deportivo. Ha trabajado para medios reconocidos como as.com, donde ha cubierto desde ligas locales hasta competiciones internacionales como la Copa del Mundo, los Juegos Olímpicos y los torneos más destacados de disciplinas como el fútbol, baloncesto y tenis.",
+    "tags": [],
+    "title": "Quién soy",
+    "uri": "/PracticaHugo1/index.html"
+  },
+  {
+    "breadcrumb": "Quién soy",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/PracticaHugo1/categories/index.html"
+  },
+  {
+    "breadcrumb": "Quién soy \u003e Markdown",
+    "content": "Imágenes Permite mostrar imágenes para mostrar algo de una manera más gráfica.\n![Markdown logo](/markdown/markdown.png)",
+    "description": "Imágenes Permite mostrar imágenes para mostrar algo de una manera más gráfica.\n![Markdown logo](/markdown/markdown.png)",
+    "tags": [],
+    "title": "Imágenes",
+    "uri": "/PracticaHugo1/markdown/imagenes/index.html"
+  },
+  {
+    "breadcrumb": "Quién soy \u003e Markdown",
+    "content": "Texto Enriquecido Formatea texto fácilmente. Ejemplos:\nNegrita: **texto** → texto Cursiva: *texto* → texto Tachado: ~~texto~~ → texto Listas Enumera una serie de elementos y hay varios tipos:\nListas con viñetas: - Elemento 1\r- Elemento 2\r- Sub-elemento 2.1 Elemento 1\nElemento 2\nSub-elemento 2.1 Listas con números:\n1. Primer elemento\r2. Segundo elemento Primer elemento Segundo elemento Enlaces Crea enlaces con esta sintaxis:\n[Síntaxis Markdown](https://markdown.es/sintaxis-markdown/) Resultado: Síntaxis Markdown",
+    "description": "Texto Enriquecido Formatea texto fácilmente. Ejemplos:\nNegrita: **texto** → texto Cursiva: *texto* → texto Tachado: ~~texto~~ → texto Listas Enumera una serie de elementos y hay varios tipos:\nListas con viñetas: - Elemento 1\r- Elemento 2\r- Sub-elemento 2.1 Elemento 1\nElemento 2\nSub-elemento 2.1 Listas con números:\n1. Primer elemento\r2. Segundo elemento Primer elemento Segundo elemento Enlaces Crea enlaces con esta sintaxis:\n[Síntaxis Markdown](https://markdown.es/sintaxis-markdown/) Resultado: Síntaxis Markdown",
+    "tags": [],
+    "title": "Otros",
+    "uri": "/PracticaHugo1/markdown/otros/index.html"
+  },
+  {
+    "breadcrumb": "Quién soy \u003e Markdown",
+    "content": "Tablas Permiten estructurar datos en filas y columnas.\n| Encabezado 1 | Encabezado 2 | Encabezado 3 |\r|--------------|--------------|--------------|\r| Dato 1 | Dato 2 | Dato 3 |\r| Dato 4 | Dato 5 | Dato 6 | Resultado:\nEncabezado 1 Encabezado 2 Encabezado 3 Dato 1 Dato 2 Dato 3 Dato 4 Dato 5 Dato 6",
+    "description": "Tablas Permiten estructurar datos en filas y columnas.\n| Encabezado 1 | Encabezado 2 | Encabezado 3 |\r|--------------|--------------|--------------|\r| Dato 1 | Dato 2 | Dato 3 |\r| Dato 4 | Dato 5 | Dato 6 | Resultado:\nEncabezado 1 Encabezado 2 Encabezado 3 Dato 1 Dato 2 Dato 3 Dato 4 Dato 5 Dato 6",
+    "tags": [],
+    "title": "Tablas",
+    "uri": "/PracticaHugo1/markdown/tablas/index.html"
+  },
+  {
+    "breadcrumb": "Quién soy",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/PracticaHugo1/tags/index.html"
+  }
+]
